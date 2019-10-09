c88d405d69a38f6c99a92de8b557dec1b65939ac
zicen <1140377034@qq.com>
2019-10-09 18:55:41 +0800

    网络请求

--- app/src/main/java/com/zicen/myandroiddemoproject/home/HomeActivity.java
+++ app/src/main/java/com/zicen/myandroiddemoproject/home/HomeActivity.java
@@ -1,10 +1,5 @@
 package com.zicen.myandroiddemoproject.home;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import com.zicen.myandroiddemoproject.R;
-
 import me.yokeyword.fragmentation.SupportActivity;
 
 public class HomeActivity extends SupportActivity {
